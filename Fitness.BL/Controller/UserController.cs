160585d54c7d5e16ce5755be0245990179c3e9e0
DavidMikutishvili <davidstandartx@gmail.com>
2021-03-23 16:42:09 +0300

    Created the initial implementation of saving and loading the user

---
+++ Fitness.BL/Controller/UserController.cs
@@ -0,0 +1,62 @@
+﻿using Fitness.BL.Model;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fitness.BL.Controller
+{
+    /// <summary>
+    /// Контроллер пользователя.
+    /// </summary>
+    public class UserController
+    {
+        /// <summary>
+        /// Пользователь приложения.
+        /// </summary>
+        public User User { get; }
+
+        /// <summary>
+        /// Создание нового контроллера пользователя.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController(string userName,
+                              string genderName,
+                              DateTime birthDay,
+                              double weight,
+                              double height)
+        {
+            // TODO: Check 
+
+            var gender = new Gender(genderName);
+            User = new User(userName, gender, birthDay, weight, height);
+        }
+
+        public UserController()
+        {
+            var formatter = new BinaryFormatter();
+
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (formatter.Deserialize(fs) is User user)
+                {
+                    User = user;
+                }
+
+                // TODO: What to do if the user is not read?
+            }
+        }
+
+        /// <summary>
+        /// Сохранить данные пользователя.
+        /// </summary>
+        public void Save()
+        {
+            var formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (var fs = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, User);
+            }
+        }
+    }
+}
--- Fitness.CMD/Program.cs
+++ Fitness.CMD/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Fitness.BL.Controller;
+using System;
 
 namespace Fitness.CMD
 {
@@ -6,6 +7,25 @@ namespace Fitness.CMD
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to the Fitness app");
+
+            Console.WriteLine("Enter the user name");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Enter your gender");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Enter your birth date");
+            var birthDate = DateTime.Parse(Console.ReadLine()); // TODO: rewrite
+
+            Console.WriteLine("Enter your weight");
+            var weight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height");
+            var height = Double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthDate, weight, height);
+            userController.Save();
         }
     }
 }
